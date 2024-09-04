ce0002c7e9d6cb24173743a91f551a99ceb1285a
mahakiim <muhammadhakim.ed@gmail.com>
2024-09-04 20:39:59 +0700

    nambahin testing mas

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 app = Flask(__name__)
 from Controller import about, home, maps
 
+#testing mas
+
 app.register_blueprint(home.home_bp)
 app.register_blueprint(about.about_bp)
 app.register_blueprint(maps.maps_bp)
