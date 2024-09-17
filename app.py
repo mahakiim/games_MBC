967dd404de7f8ed3c24bbf6216d33c2819fe2d3f
Muhammad Hakim <139327011+mahakiim@users.noreply.github.com>
2024-09-17 15:16:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-from controller import about, home, maps, gaada
+from controller import about, home, maps, gaada, hehe
 
 #testing mas
 
@@ -10,6 +10,7 @@ app.register_blueprint(home.home_bp)
 app.register_blueprint(about.about_bp)
 app.register_blueprint(maps.maps_bp)
 app.register_blueprint(gaada.gaada_bp)
+app.register_blueprint(hehe.hehe_bp)
 
 @app.route('/')
 def index():
